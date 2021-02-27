1fc585481f81e38817e075e88989a6e89a0f89e7
yitongli <yitongliphysics@gmail.com>
2021-02-27 16:36:50 +0800

    Update Exercise8.5.py

--- Exercise8.5.py
+++ Exercise8.5.py
@@ -16,3 +16,21 @@ for line in fh:
 
 
 print("There were", count, "lines in the file with From as the first word")
+
+#Another way to complete this exereise
+
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('From '): continue
+    words = line.split()
+    count = count + 1
+    print(words[1])
+
+print("There were", count, "lines in the file with From as the first word")
+
